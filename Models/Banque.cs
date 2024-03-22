92138d16704e17c4e3a1994feea831ce17c76299
Thierry Morre <thierry.morre@cognitic.be>
2024-03-22 14:10:20 +0100

    Implémentation de la classe Banque et d'lindexeur

---
+++ Models/Banque.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Banque
+    {
+        private Dictionary<string, Courant> _comptes = new Dictionary<string, Courant>();
+
+        public string Nom { get; set; }
+
+        public Courant? this[string numero]
+        {
+            get
+            {
+                if(!_comptes.ContainsKey(numero))
+                    return null;
+
+                return _comptes[numero];
+            }
+        }
+
+        public void Ajouter(Courant compte)
+        {
+            _comptes.Add(compte.Numero, compte);
+        }
+
+        public void Supprimer(string numero)
+        {
+            if (!_comptes.ContainsKey(numero))
+                return;
+
+            _comptes.Remove(numero);
+        }
+    }
+}
