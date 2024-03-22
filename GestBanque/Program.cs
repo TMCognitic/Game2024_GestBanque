29111b42d4e40922eebeea9efd29d64a270f9ba6
Thierry Morre <thierry.morre@cognitic.be>
2024-03-22 10:48:12 +0100

    Ajout des classes Personne et Courant

--- GestBanque/Program.cs
+++ GestBanque/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Models;
+
+namespace GestBanque;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Personne doeJohn = new Personne()
+        {
+            Nom = "Doe",
+            Prenom = "John",
+            DateNaiss = new DateTime(1970, 1, 1)
+        };
+
+        Courant courant = new Courant()
+        {
+            Numero = "0001",
+            LigneDeCredit = 500,
+            Titulaire = doeJohn
+        };
+
+        courant.Depot(-100);
+        Console.WriteLine($"Depot de -100 : {courant.Solde}");
+        courant.Depot(100);
+        Console.WriteLine($"Depot de 100 : {courant.Solde}");
+        courant.Retrait(-100);
+        Console.WriteLine($"Retrait de -100 : {courant.Solde}");
+        courant.Retrait(100);
+        Console.WriteLine($"Retrait de 100 : {courant.Solde}");
+        courant.Retrait(600);
+        Console.WriteLine($"Retrait de 600 : {courant.Solde}");
+    }
+}
