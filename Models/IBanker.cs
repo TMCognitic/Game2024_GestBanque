83bb36682b2f99c2a6896217822fe51470c68c2b
Thierry Morre <thierry.morre@cognitic.be>
2024-03-26 15:33:23 +0100

    Petite correction dans IBanker

--- Models/IBanker.cs
+++ Models/IBanker.cs
@@ -2,9 +2,8 @@
 {
     public interface IBanker
     {
-        string Numero { get; set; }
-        Personne Titulaire { get; set; }
-
+        string Numero { get; }
+        Personne Titulaire { get; }
         void AppliquerInteret();
     }
 }
