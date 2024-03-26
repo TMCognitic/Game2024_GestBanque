61c585c31389c388da56517816710c36cc5ca0a3
Thierry Morre <thierry.morre@cognitic.be>
2024-03-26 15:29:49 +0100

    Exercices sur les interfaces

---
+++ Models/IBanker.cs
@@ -0,0 +1,10 @@
+﻿namespace Models
+{
+    public interface IBanker
+    {
+        string Numero { get; set; }
+        Personne Titulaire { get; set; }
+
+        void AppliquerInteret();
+    }
+}
